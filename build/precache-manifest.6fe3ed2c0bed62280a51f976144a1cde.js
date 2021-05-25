7d48fa8a7653d71bc17ebc3e5c2ce5a22490bbda
Sergey <execute25@gmail.com>
2021-05-25 16:57:02 +0900

    Git ignore update

---
+++ build/precache-manifest.6fe3ed2c0bed62280a51f976144a1cde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330e3ae0ce190b3ec95c4d77cd263571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "068cc706d8a7ba8b63b3",
+    "url": "/static/css/2.f3d32d52.chunk.css"
+  },
+  {
+    "revision": "ab61d06c985598b04de8",
+    "url": "/static/css/main.a963acd6.chunk.css"
+  },
+  {
+    "revision": "068cc706d8a7ba8b63b3",
+    "url": "/static/js/2.30bbd348.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/static/js/2.30bbd348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab61d06c985598b04de8",
+    "url": "/static/js/main.d8b57154.chunk.js"
+  },
+  {
+    "revision": "4e8d83cc71677486d72a",
+    "url": "/static/js/runtime-main.7eae750c.js"
+  }
+]);
